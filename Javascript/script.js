4c760acd150017688893ccd460e8fdd5df8cc265
V2na <danieltoth706@gmail.com>
2025-12-07 12:42:13 +0100

    Version1

---
+++ Javascript/script.js
@@ -0,0 +1,34 @@
+
+// Scroll to top button
+let mybutton = document.getElementById("btn-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.classList.add("show");
+  } else {
+    mybutton.classList.remove("show");
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+// Make navbar closeable
+const navLinks = document.querySelectorAll('.navbar-collapse .nav-link');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    if (navbarCollapse.classList.contains('show')) {
+      new bootstrap.Collapse(navbarCollapse).toggle();
+    }
+  });
+});
+
+var modal = new bootstrap.Modal('#myModal');
+modal.show();
